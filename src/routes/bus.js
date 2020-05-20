81848f076190a8c8ea8f40fbb04dc7a603fceedb
Rafael S. L <rafael.souza.lana@gmail.com>
2020-05-20 19:53:03 -0300

    Updates the schedules every week, creates logs and check for updates

--- src/routes/bus.js
+++ src/routes/bus.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 import axios from 'axios';
 import cheerio from 'cheerio';
 import https from 'https';
 var iconv = require('iconv-lite');
+import { CronJob } from "cron";
+import moment from "moment";
+import 'moment/locale/pt-br';
+moment.locale('pt-BR');
 import { loadData, storeData } from "../../database/storage";
 
 const router = express.Router();
@@ -28,15 +33,48 @@ api.interceptors.response.use(response => {
   return response;
 })
 
+// Documentaion: https://github.com/kelektiv/node-cron#readme
+// Scrape the schedules, create a backup and update the buses.json file
+// Every saturday at 02:00 AM
+const cronScrapeAndUpdate = new CronJob('00 00 02 * * 6', async () => {
+  // const cronScrapeAndUpdate = new CronJob('10 50 19 * * *', async () => {
+  const logDate = `----- ${new Date()} -----\r\n`;
+  try {
+    console.log("🤞 Scraping...", new Date());
+    await scrape();
+    console.log("✌️ That's all folks");
+
+    const successes = fs.readFileSync(path.join(__dirname, "../../logs/successes.log"), "utf8");
+    fs.writeFileSync(
+      path.join(__dirname, "../../logs/successes.log"),
+      successes + logDate + `Update success` + "\r\n\r\n"
+    );
+  } catch (error) {
+    const errors = fs.readFileSync(path.join(__dirname, "../../logs/errors.log"), "utf8");
+    fs.writeFileSync(
+      path.join(__dirname, "../../logs/errors.log"),
+      errors + logDate + error.message + "\r\n\r\n"
+    );
+  }
+}, null, true, 'America/Sao_Paulo');
+
+cronScrapeAndUpdate.start();
+
 const scrape = async () => {
   try {
-    const oldBuses = JSON.parse(
-      loadData(path.join(__dirname, "../../database/linhas.json"))
+    const busNumbers = JSON.parse(
+      loadData(path.join(__dirname, "../../database/numbers.json"))
     );
-    const busNumbers = oldBuses.map(bus => bus.number);
 
-    console.log("🤞 Scraping...");
-    const newBuses = await busNumbers.map(async busNumber => {
+    const today = moment().format('YYYY-MM-DD');
+
+    fs.copyFileSync(
+      path.join(__dirname, "../../database/buses.json"),
+      path.join(__dirname, `../../database/backups/buses-${today}.json`));
+
+    storeData([], path.join(__dirname, `../../database/buses.json`));
+
+    const newBuses = await Promise.all(busNumbers.map(async busNumber => {
       const url = `${BASE_URL}/onibus/itinerario/index.php?psq_lin=num&psq_txtval=${busNumber}`;
 
       const responseAxios = await api.post(url);
@@ -123,27 +161,58 @@ const scrape = async () => {
       console.log(`✅  ${number} - ${name}`);
 
       const buses = JSON.parse(
-        loadData(path.join(__dirname, "../../database/buses-2020-05-19.json"))
+        loadData(path.join(__dirname, "../../database/buses.json"))
       );
 
       storeData(
         [...buses, bus],
-        path.join(__dirname, "../../database/buses-2020-05-19.json")
+        path.join(__dirname, "../../database/buses.json")
       );
       return bus;
-    });
+    }));
+    storeData({
+      "lastUpdateDate": new Date().toISOString()
+    }, path.join(__dirname, `../../database/lastUpdateDate.json`));
     return newBuses;
   } catch (error) {
     console.log(`❌  ${error}`);
   }
 }
 
-// router.get('/', async (req, res) => {
-//   try {
-//     // To be continued...
-//   } catch (err) {
-//     return res.status(400).send({ errors: err.message });
-//   }
-// });
+const checkUpdate = async lastUpdateDate => {
+  const clientLastUpdateDate = new Date(lastUpdateDate || 0);
+  const storage = await JSON.parse(
+    loadData(path.join(__dirname, "../../database/lastUpdateDate.json"))
+  );
+  const serverLastUpdateDate = new Date(storage.lastUpdateDate);
+  const needUpdate = serverLastUpdateDate.getTime() > clientLastUpdateDate.getTime();
+  return needUpdate;
+}
+
+router.post('/checkUpdate', async (req, res) => {
+  try {
+    const { lastUpdateDate } = req.body;
+    const needUpdate = await checkUpdate(lastUpdateDate);
+    return res.status(200).send({ needUpdate });
+  } catch (err) {
+    return res.status(400).send({ error: err.message });
+  }
+});
+
+router.post('/update', async (req, res) => {
+  try {
+    const { lastUpdateDate } = req.body;
+    const needUpdate = await checkUpdate(lastUpdateDate);
+    if (!needUpdate) throw new Error('Seus horários já estão atualizados');
+
+    const buses = await JSON.parse(
+      loadData(path.join(__dirname, "../../database/buses.json"))
+    );
+
+    return res.status(200).send({ buses });
+  } catch (err) {
+    return res.status(400).send({ error: err.message });
+  }
+});
 
 module.exports = app => app.use('/api/bus', router);
